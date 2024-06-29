cda0c455de5367b7df9b6edb2e424a3a29f9cca8
Harsh Pandey <pandey73harsh@gmail.com>
2024-06-30 03:27:44 +0530

    add seat no.

--- app/(dashboard)/(routes)/buy/[slug]/Seat.jsx
+++ app/(dashboard)/(routes)/buy/[slug]/Seat.jsx
@@ -2,11 +2,12 @@
 import React from 'react'
 import { FaCheck } from "react-icons/fa6";
 
-const Seat = ({isBooked, selected}) => {
+const Seat = ({isBooked, selected, setSelected, index}) => {
 
   return (
-    <div className=' w-5 h-5 text-sm grid place-items-center rounded-full border border-slate-300 hover:bg-slate-500  ' style={{backgroundColor: isBooked? "rgb(31 41 55)" : ""}} >
-      {selected && <FaCheck />}
+    <div onClick={()=>setSelected(index)} className=' w-5 h-5 text-sm grid place-items-center rounded-full border border-slate-300 hover:bg-slate-500  ' style={{backgroundColor: isBooked? "rgb(31 41 55)" : ""}} >
+      {selected===index ? <FaCheck />: ""  }
+      {/* {selected && <FaCheck />} */}
     </div>
   )
 }
--- app/(dashboard)/(routes)/buy/[slug]/page.jsx
+++ app/(dashboard)/(routes)/buy/[slug]/page.jsx
@@ -5,7 +5,7 @@ import Seat from './Seat'
 const page = ({params}) => {
     const [isBooked, setIsBooked] = useState(false );
     const [maxCapacity, setMaxCapacity] = useState(50);
-    const [selected, setSelected] = useState(false);
+    const [selected, setSelected] = useState(null);
   return (
     <div className=' w-fit h-full text-white text-3xl grid place-items-center mx-auto p-20 ' >
         <h1 className='text-3xl font-bold text-left my-5 '>
@@ -14,9 +14,13 @@ const page = ({params}) => {
       <div className=' w-full border border-dashed rounded-lg h-full '  >
         <div className=' grid grid-cols-4 gap-4 place-items-center py-4 '>
       {Array.from({ length: maxCapacity }, (_, index) =>(
-          <Seat isBooked={isBooked} key={index} selected={selected}  />
+          <Seat isBooked={isBooked} key={index} selected={selected} setSelected={setSelected} index={index}  />
         ))}
         </div>
+
+        { selected && <h3 className='text-sm'>
+          Selected seat: {selected}
+        </h3>}
       </div>
     </div>
   )
