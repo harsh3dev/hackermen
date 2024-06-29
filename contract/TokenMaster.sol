03833b1a2a0fde97a98cd4e2db43ac6a6d4fa5de
Harsh Pandey <pandey73harsh@gmail.com>
2024-06-29 21:04:18 +0530

    new push

--- contract/TokenMaster.sol
+++ contract/TokenMaster.sol
@@ -2,36 +2,40 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 
 contract TokenMaster is ERC721 {
-    address public owner; //added
+    address public owner;
     uint256 public totalOccasions;
     uint256 public totalSupply;
-    
-    struct Ticket {
-        uint256 id;
-        uint256 price;
-        address originalOwner;
+
+    struct Ticket{
+        address payable owner;
+        uint256 occasionId;
+        uint256 seatNo;
     }
 
     struct Occasion {
+        address eventOrganiser;
         uint256 id;
         string name;
         uint256 cost;
-        uint256[] ticketIds;
+        uint256 tickets;
         uint256 maxTickets;
         string date;
         string time;
         string location;
-        address owner
+        uint256 percentage;
     }
 
     mapping(uint256 => Occasion) occasions;
     mapping(uint256 => mapping(address => bool)) public hasBought;
     mapping(uint256 => mapping(uint256 => address)) public seatTaken;
     mapping(uint256 => uint256[]) seatsTaken;
+    mapping(uint256 => string) private _tokenURIs;
+    mapping(uint256 => Ticket) public tickets;
+    mapping(uint256 => uint256) public ticketListing;
+    mapping(uint256 => bool) public isticketListed;
+
 
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -45,61 +49,122 @@ contract TokenMaster is ERC721 {
         owner = msg.sender;
     }
 
-    function list(
+    function listOccasion(
         string memory _name,
         uint256 _cost,
         uint256 _maxTickets,
         string memory _date,
         string memory _time,
-        string memory _location
-    ) public  {
+        string memory _location,
+        uint256 _percentage
+    ) public onlyOwner {
         totalOccasions++;
-        Occasion storage newEvent = occasion[totalOccasions];
-        newEvent.id=totalOccasions,
-        newEvent.name=_name,
-        for(uint256 i = 0; i < ticketPrices.length; i++){
-        ticketCounter++;
-        uint256 ticketId = ticketCounter;
-        _mint(msg.sender, ticketId);  
-        tickets[ticketId] = Ticket(ticketId, ticketPrices[i], msg.sender, qrCodeURIs[i]);
-        newEvent.ticketIds.push(ticketId);
-        }
-        newEvent.date=_date,
-        newEvent.time=_time,
-        newEvent.location=_location,
-        newEvent.owner= msg.sender
+        occasions[totalOccasions] = Occasion(
+            msg.sender,
+            totalOccasions,
+            _name,
+            _cost,
+            _maxTickets,
+            _maxTickets,
+            _date,
+            _time,
+            _location,
+            _percentage
+        );
     }
 
-    function mint(uint25600 _id, uint256 _seat) public payable {
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function mint(uint256 _Occasion_id, uint256 _seat, string memory ipfsLink) public payable {
         // Require that _id is not 0 or less than total occasions...
-        require(_id != 0);
-        require(_id <= totalOccasions);
+        require(_Occasion_id != 0);
+        require(_Occasion_id <= totalOccasions);
 
         // Require that ETH sent is greater than cost...
-        require(msg.value >= occasions[_id].cost);
+        require(msg.value >= occasions[_Occasion_id].cost);
 
         // Require that the seat is not taken, and the seat exists...
-        require(seatTaken[_id][_seat] == address(0));
-        require(_seat <= occasions[_id].maxTickets);
+        require(seatTaken[_Occasion_id][_seat] == address(0));
+        require(_seat <= occasions[_Occasion_id].maxTickets);
 
-        occasions[_id].tickets -= 1; // <-- Update ticket count
+        occasions[_Occasion_id].tickets -= 1; // <-- Update ticket count
 
-        hasBought[_id][msg.sender] = true; // <-- Update buying status
-        seatTaken[_id][_seat] = msg.sender; // <-- Assign seat
+        hasBought[_Occasion_id][msg.sender] = true; // <-- Update buying status
+        seatTaken[_Occasion_id][_seat] = msg.sender; // <-- Assign seat
 
-        seatsTaken[_id].push(_seat); // <-- Update seats currently taken
+        seatsTaken[_Occasion_id].push(_seat); // <-- Update seats currently taken
 
         totalSupply++;
 
+        tickets[totalSupply] = Ticket(
+            payable(msg.sender),
+            _Occasion_id,
+            _seat
+        );
+
+        isticketListed[totalSupply] = false;
+
+        (bool success, ) = occasions[_Occasion_id].eventOrganiser.call{value:msg.value}("");
+        require(success, "Failed to transfer the funds");
+
         _safeMint(msg.sender, totalSupply);
+        _setTokenURI(totalSupply, ipfsLink);
     }
 
-    function getOccasion(uint256 _id) public view returns (Occasion memory) {
-        return occasions[_id];
+    function TicketListing(uint256 _tokenId, uint256 _price) public {
+        require(tickets[_tokenId].owner == msg.sender);
+        require(_price > 0 , "Price should be more than 0");
+        ticketListing[_tokenId] = _price;
+        isticketListed[_tokenId] = true;
     }
 
-    function getSeatsTaken(uint256 _id) public view returns (uint256[] memory) {
-        return seatsTaken[_id];
+    function transferTicket(uint256 _tokenId, uint256 price) public payable {
+        address payable tokenOwner = tickets[_tokenId].owner; 
+        require(isticketListed[_tokenId] == true, "The token is not listed for sale");
+        
+        uint256 orgPrice = (ticketListing[_tokenId] * occasions[tickets[_tokenId].occasionId].percentage) / 100;    
+        uint256 actualPrice = ticketListing[_tokenId] + orgPrice;
+
+        require(price >= actualPrice, "The price is not enough");
+        require(tokenOwner != msg.sender, "Owner cannot the ticket");
+        require(ticketListing[_tokenId] > 0, "The price cannot be set to 0");
+        
+        (bool success, ) = tokenOwner.call{value:(price-orgPrice)}("");
+        require(success, "Failed to transfer the funds");
+
+        (bool success2, ) = occasions[tickets[_tokenId].occasionId].eventOrganiser.call{value:orgPrice}("");
+        require(success2, "Failed to transfer the funds");
+        
+        _transfer(tokenOwner, msg.sender, _tokenId);
+        ticketListing[_tokenId] = 0;
+        isticketListed[_tokenId] = false;
+        tickets[_tokenId].owner = payable(msg.sender);
+    }
+
+    function isTicketListed(uint256 _tokenId) public view returns (bool)
+    {
+        return isticketListed[_tokenId];
+    }
+
+    function getIpfsLink(uint256 _tokenId) public view returns (string memory)
+    {
+        return _tokenURIs[_tokenId];
+    }
+
+    function getTicketOwner(uint256 _tokenId) public view returns (address)
+    {
+        return tickets[_tokenId].owner;
+    }
+
+
+    function getOccasion(uint256 _Occasion_id) public view returns (Occasion memory) {
+        return occasions[_Occasion_id];
+    }
+
+    function getSeatsTaken(uint256 _Occasion_id) public view returns (uint256[] memory) {
+        return seatsTaken[_Occasion_id];
     }
 
     function withdraw() public onlyOwner {
