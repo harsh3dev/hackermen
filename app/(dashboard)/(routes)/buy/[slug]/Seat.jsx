93b2c334ed7cd532869e31071eb39c925145d74e
Harsh Pandey <pandey73harsh@gmail.com>
2024-06-30 03:16:41 +0530

    fix error in seat

--- app/(dashboard)/(routes)/buy/[slug]/Seat.jsx
+++ app/(dashboard)/(routes)/buy/[slug]/Seat.jsx
@@ -5,7 +5,7 @@ import { FaCheck } from "react-icons/fa6";
 const Seat = ({isBooked, selected}) => {
 
   return (
-    <div onClick={()=>setSelected((prev)=>(!prev))} className=' w-5 h-5 text-sm grid place-items-center rounded-full border border-slate-300 hover:bg-slate-500  ' style={{backgroundColor: isBooked? "rgb(31 41 55)" : ""}} >
+    <div className=' w-5 h-5 text-sm grid place-items-center rounded-full border border-slate-300 hover:bg-slate-500  ' style={{backgroundColor: isBooked? "rgb(31 41 55)" : ""}} >
       {selected && <FaCheck />}
     </div>
   )
